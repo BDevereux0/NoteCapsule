9d383451c1fb56b31e4982960f7503e6cff4959b
BDevereux0 <BDevereux0@gmail.com>
2026-07-19 21:56:13 -0400

    Scaffolding and .gitignore added

---
+++ src/main/java/com/notecapsule/NoteCapsuleApplication.java
@@ -0,0 +1,12 @@
+package com.notecapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteCapsuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteCapsuleApplication.class, args);
+    }
+}
